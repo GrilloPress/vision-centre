0698cdd11cb19fc1d52a0ab34ff8db95ff20440a
GrilloPress <grillopress@gmail.com>
2015-05-20 13:20:50 +0000

    scaffold site

---
+++ config.rb
@@ -0,0 +1,13 @@
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+
+configure :build do
+
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  set :relative_links, true
+  activate :livereload
+end
